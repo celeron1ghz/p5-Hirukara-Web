1784941e157629d53121c10a391b43a984e71c77
celeron1ghz <celeron1ghz@gmai.com>
2014-08-02 22:06:30 +0900

    added circle type

--- lib/Hirukara/Constants/CircleType.pm
+++ lib/Hirukara/Constants/CircleType.pm
@@ -6,6 +6,8 @@ my %LOOKUP = (
     1 => { class => "gohairyo", value => 1, label => 'ご配慮' },
     2 => { class => "families", value => 2, label => '身内' },
     3 => { class => "absent",   value => 3, label => '欠席' },
+    4 => { class => "confirm",  value => 4, label => '要確認' },
+    5 => { class => "malonu",   value => 5, label => 'ﾇﾇﾝﾇ' },
 );
 
 sub lookup  {
