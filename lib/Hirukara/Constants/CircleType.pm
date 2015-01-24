eadb948ddf52b239f5802ea9c95806ee2f3b8162
celeron1ghz <celeron1ghz@gmail.com>
2015-01-24 13:13:06 +0900

    changed label circle_type style on circle desc page

--- lib/Hirukara/Constants/CircleType.pm
+++ lib/Hirukara/Constants/CircleType.pm
@@ -4,17 +4,17 @@ use strict;
 use warnings;
 
 my %LOOKUP = (
-    1  => { class => "gohairyo",    value => 1,  label => 'ご配慮' },
-    2  => { class => "families",    value => 2,  label => '身内' },
-    3  => { class => "absent",      value => 3,  label => '欠席' },
-    4  => { class => "confirm",     value => 4,  label => '要確認' },
-    5  => { class => "malonu",      value => 5,  label => 'ﾇﾇﾝﾇ' },
-    6  => { class => "noshinkan",   value => 6,  label => '新刊なし' },
-    50 => { class => "fixed",       value => 50, label => 'fix α' },
-    51 => { class => "fixed",       value => 51, label => 'fix β' },
-    52 => { class => "fixed",       value => 52, label => 'fix γ' },
-    53 => { class => "fixed",       value => 53, label => 'fix ω' },
-    99 => { class => "deprecated",  value => 99, label => 'エラーデータ' },
+    1  => { class => "info",    value => 1,  label => 'ご配慮' },
+    2  => { class => "success", value => 2,  label => '身内' },
+    3  => { class => "default", value => 3,  label => '欠席' },
+    4  => { class => "warning", value => 4,  label => '要確認' },
+    5  => { class => "danger",  value => 5,  label => 'ﾇﾇﾝﾇ' },
+    6  => { class => "default", value => 6,  label => '新刊なし' },
+    50 => { class => "info",    value => 50, label => 'fix α' },
+    51 => { class => "info",    value => 51, label => 'fix β' },
+    52 => { class => "info",    value => 52, label => 'fix γ' },
+    53 => { class => "info",    value => 53, label => 'fix ω' },
+    99 => { class => "default", value => 99, label => 'エラーデータ' },
 );
 
 sub lookup  {
