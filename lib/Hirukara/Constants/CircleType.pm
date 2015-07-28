88af39b09934e86cc54c39ca92e74578aec95c36
celeron1ghz <celeron1ghz@gmail.com>
2015-07-28 22:04:21 +0900

    new circle_type added

--- lib/Hirukara/Constants/CircleType.pm
+++ lib/Hirukara/Constants/CircleType.pm
@@ -16,6 +16,7 @@ my %LOOKUP = (
     53 => { class => "info",    value => 53, label => 'fix ω' },
     54 => { class => "info",    value => 54, label => 'ご配慮(7/26〆)' },
     55 => { class => "info",    value => 55, label => 'ご配慮(8/02〆)' },
+    56 => { class => "info",    value => 56, label => 'ご配慮(7/31〆)' },
     99 => { class => "default", value => 99, label => 'エラーデータ' },
 );
 
