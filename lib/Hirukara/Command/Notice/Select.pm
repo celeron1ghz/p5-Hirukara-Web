7e353f53e9b83c963e099e899cd3844a160b5923
celeron1ghz <celeron1ghz@gmail.com>
2015-07-29 20:10:25 +0900

    changed notice display term

--- lib/Hirukara/Command/Notice/Select.pm
+++ lib/Hirukara/Command/Notice/Select.pm
@@ -5,8 +5,10 @@ with 'MooseX::Getopt', 'Hirukara::Command';
 
 sub run {
     my $self = shift;
+    my $time = time - 60 * 60 * 24 * 60;
     [ $self->database->search('notice' => {
-        created_at => \'= (SELECT MAX(created_at) FROM notice n WHERE notice.key = n.key)'
+        created_at => \'= (SELECT MAX(created_at) FROM notice n WHERE notice.key = n.key)',
+        key        => { '>=' => $time },  ## TODO: should change create_at to epoch value
     }, {
         order_by => "key DESC",
     })->all ];
