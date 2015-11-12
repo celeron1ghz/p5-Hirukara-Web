ba390445b6d2c77ba7ae90c37ce0424ffd900387
celeron1ghz <celeron1ghz@gmail.com>
2015-11-12 20:31:11 +0900

    simply port twitter auth

--- Hirukara/lib/Hirukara/Web.pm
+++ Hirukara/lib/Hirukara/Web.pm
@@ -5,6 +5,9 @@ use utf8;
 use parent qw/Hirukara Amon2::Web/;
 use File::Spec;
 
+use Log::Minimal;
+use Net::Twitter::Lite::WithAPIv1_1;
+
 # dispatcher
 use Hirukara::Web::Dispatcher;
 sub dispatch {
@@ -14,9 +17,41 @@ sub dispatch {
 # load plugins
 __PACKAGE__->load_plugins(
     'Web::FillInFormLite',
-    'Web::JSON',
+                'Web::Auth' => { module => 'Twitter', on_finished => \&_twitter_auth_successed },
     '+Hirukara::Web::Plugin::Session',
 );
+
+sub _twitter_auth_successed {
+    my($c,$access_token,$access_secret,$user_id,$screen_name) = @_;
+    infof "LOGIN_SUCCESS: user_id=%s, screen_name=%s", $user_id, $screen_name;
+
+    my $conf = $c->config->{Auth}->{Twitter};
+    my $n = Net::Twitter::Lite::WithAPIv1_1->new(
+        consumer_key    => $conf->{consumer_key},
+        consumer_secret => $conf->{consumer_secret},
+        ssl             => 1,
+    );
+
+    $n->access_token($access_token);
+    $n->access_token_secret($access_secret);
+
+    my $me   = $n->verify_credentials;
+    my $id   = $me->{id};
+    my $name = $me->{name};
+    my $image_url = $me->{profile_image_url};
+    $image_url =~ s/^http/https/;
+
+    $c->session->set(user => {
+        member_id         => $screen_name,
+        member_name       => $name,
+        id                => $id,
+        profile_image_url => $image_url,
+    });
+
+    my $ua = $c->req->headers->header('User-Agent');
+    my $ip = $c->req->address;
+    $c->redirect("/");
+}
 
 # setup view
 use Hirukara::Web::View;
