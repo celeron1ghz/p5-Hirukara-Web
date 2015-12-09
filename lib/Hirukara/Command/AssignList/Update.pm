92b9db6caa07304144c60b9ef4f94092bfd52e12
celeron1ghz <celeron1ghz@gmail.com>
2015-12-09 22:53:28 +0900

    fixed UUV

--- lib/Hirukara/Command/AssignList/Update.pm
+++ lib/Hirukara/Command/AssignList/Update.pm
@@ -13,13 +13,13 @@ sub run {
     my $self = shift;
     my $id            = $self->assign_id;
     my $member_id     = $self->member_id;
-    my $assign_member = $self->assign_member_id;
-    my $assign_name   = $self->assign_name || '';
+    my $assign_member = $self->assign_member_id || '';
+    my $assign_name   = $self->assign_name      || '';
     my $assign        = $self->db->single(assign_list => { id => $id });
     my $updated_value = {};
+    my $before_assign_member = $assign->member_id || '';
 
-    if ($assign_member ne $assign->member_id) {
-        my $before_assign_member = $assign->member_id || '';
+    if ($assign_member ne $before_assign_member) {
         $updated_value->{member_id} = $assign_member;
 
         $self->actioninfo('割り当てリストのメンバーを更新しました。' =>
