2c07c81cfa7f8d8b6b103daa79829311b5f90d8a
celeron1ghz <celeron1ghz@gmail.com>
2015-12-29 21:28:20 +0900

    fixed in admin.bulk_export

--- lib/Hirukara/Command/Admin/BulkExport.pm
+++ lib/Hirukara/Command/Admin/BulkExport.pm
@@ -16,7 +16,7 @@ use Hirukara::Command::CircleOrder::Export::ComiketCsv;
 
 with 'MooseX::Getopt', 'Hirukara::Command', 'Hirukara::Command::Exhibition';
 
-has member_id => ( is => 'ro', isa => 'Str', required => 1 );
+has run_by => ( is => 'ro', isa => 'Str', required => 1 );
 
 sub run {
     my $self    = shift;
@@ -24,14 +24,6 @@ sub run {
     my @lists   = $self->db->search('assign_list' => { comiket_no => $e })->all;
     my $tempdir = path(tempdir());
     my $start   = time;
-
-    $self->actioninfo("チェックリストの一括出力を行います。" => 
-        exhibition       => $e,
-        member_id         => $self->member_id,
-        assign_list_count => scalar @lists,
-        dir               =>"$tempdir",
-    );
-
     my @jobs;
 
     for my $member ($self->db->select('member')->all)   {
@@ -39,7 +31,8 @@ sub run {
             object => Hirukara::Command::CircleOrder::Export::OrderPdf->new(
                 hirukara   => $self->hirukara,
                 exhibition => $self->hirukara->exhibition,
-                member_id  => $member->member_id
+                member_id  => $member->member_id,
+                run_by     => $self->run_by,
             ),
             dest => $tempdir->path(sprintf "%s [ORDER].pdf", $member->member_id),
         };
@@ -56,9 +49,10 @@ sub run {
 
         push @jobs, {
             object => Hirukara::Command::CircleOrder::Export::BuyPdf->new(
-                hirukara       => $self->hirukara,
-                exhibition     => $self->hirukara->exhibition,
-                where          => Hash::MultiValue->new(assign => $list->id),
+                hirukara   => $self->hirukara,
+                exhibition => $self->hirukara->exhibition,
+                where      => Hash::MultiValue->new(assign => $list->id),
+                run_by     => $self->run_by,
             ),
             dest => $tempdir->path(sprintf "%s (%s) [BUY].pdf", $name, $member_id),
         },{
@@ -66,17 +60,25 @@ sub run {
                 hirukara       => $self->hirukara,
                 exhibition     => $self->hirukara->exhibition,
                 assign_list_id => $list->id,
+                run_by         => $self->run_by,
             ),
             dest => $tempdir->path(sprintf "%s (%s) [DISTRIBUTE].pdf", $name, $member_id),
         },{
             object => Hirukara::Command::CircleOrder::Export::ComiketCsv->new(
-                hirukara       => $self->hirukara,
-                exhibition     => $self->hirukara->exhibition,
-                where          => Hash::MultiValue->new(assign => $list->id),
+                hirukara   => $self->hirukara,
+                exhibition => $self->hirukara->exhibition,
+                where      => Hash::MultiValue->new(assign => $list->id),
+                run_by     => $self->run_by,
             ),
             dest => $tempdir->path(sprintf "%s (%s).csv", $name, $member_id),
         };
     }
+
+    $self->actioninfo("チェックリストの一括出力を行います。" => 
+        exhibition => $e,
+        list_count => scalar @jobs,
+        run_by     => $self->run_by,
+    );
 
     my $pm  = Parallel::ForkManager->new(8);
     my $zip = Archive::Zip->new;
@@ -100,7 +102,12 @@ sub run {
     my $archive = File::Temp::tempnam(tempdir(), "hirukara");
     $zip->writeToFileNamed($archive);
     my $end = time;
-    $self->actioninfo("チェックリストの一括出力を行います。", path => $archive, elpased => $end - $start);
+    $self->actioninfo("チェックリストの一括出力を行いました。", => 
+        exhibition => $e,
+        list_count => scalar @jobs,
+        run_by     => $self->run_by,
+        elpased    => $end - $start
+    );
 
     return $archive;
 }
