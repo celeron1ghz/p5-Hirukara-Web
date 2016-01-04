319fe22d5040c00b7127ce1ffc673ef1d0b01205
celeron1ghz <celeron1ghz@gmail.com>
2016-01-04 21:06:41 +0900

    fixed list is not showing that list have no assign circle

--- lib/Hirukara/Command/Assign/Search.pm
+++ lib/Hirukara/Command/Assign/Search.pm
@@ -13,25 +13,41 @@ sub run {
     $where->{'assign_list.comiket_no'} = $self->hirukara->exhibition if $self->hirukara->exhibition;
     $where->{'assign_list.member_id'}  = $self->member_id  if $self->member_id;
 
+    my $inner = <<SQL;
+SELECT 
+    assign_list.id,
+    COUNT(DISTINCT assign.id)                   AS assign_count,
+    COUNT(DISTINCT circle.id)                   AS circle_count,
+    SUM(circle_order.count)                     AS book_count,
+    SUM(circle.circle_point)                    AS point,
+    SUM(circle_book.price * circle_order.count) AS book_price
+FROM circle
+    INNER JOIN assign        ON circle.id = assign.circle_id
+    INNER JOIN assign_list   ON assign_list.id = assign.assign_list_id
+    INNER JOIN circle_book   ON circle.id = circle_book.circle_id
+    INNER JOIN circle_order  ON circle_order.book_id = circle_book.id
+GROUP BY assign_list.id
+SQL
+
     my($sql,@binds) = $builder->select(undef, [
         [ 'assign_list.id' ],
         [ 'assign_list.name' ],
         [ 'assign_list.member_id' ],
-        [ 'member.member_name' ],
         [ 'assign_list.comiket_no' ],
         [ 'assign_list.created_at' ],
-        [ \'COUNT(DISTINCT assign.id)'  => 'assign_count' ],
-        [ \'COUNT(DISTINCT circle.id)'  => 'circle_count' ],
-        [ \'SUM(circle_order.count)'    => 'book_count' ],
-        [ \'SUM(circle.circle_point)'   => 'point' ],
-        [ \'SUM(circle_book.price * circle_order.count)' => 'book_price' ],
+
+        [ 'member.member_name' ],
+
+        [ 'counted.assign_count' ],
+        [ 'counted.circle_count' ],
+        [ 'counted.book_count' ],
+        [ 'counted.book_price' ],
+        [ 'counted.point' ],
     ], $where, {
         joins => [
-            [ assign_list => { table => 'assign',       condition => 'assign_list.id = assign.assign_list_id', type => 'LEFT' }], 
-            [ assign_list => { table => 'member',       condition => 'assign_list.member_id = member.member_id', type => 'LEFT' }], 
-            [ assign_list => { table => 'circle',       condition => 'assign.circle_id = circle.id', type => 'LEFT' }],
-            [ assign_list => { table => 'circle_book',  condition => 'circle.id = circle_book.circle_id', type => 'INNER' }],
-            [ assign_list => { table => 'circle_order', condition => 'circle_book.id = circle_order.book_id', type => 'INNER' }],
+            [ assign_list => { table => 'assign',            condition => 'assign_list.id = assign.assign_list_id', type => 'LEFT' }], 
+            [ assign_list => { table => 'member',            condition => 'assign_list.member_id = member.member_id', type => 'LEFT' }], 
+            [ assign_list => { table => \"($inner) counted", condition => 'assign_list.id = counted.id', type => 'LEFT' }],
         ],  
         group_by => 'assign_list.id',
         order_by => 'assign_list.name ASC',
